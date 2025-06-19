f07241e0d268a5d7945e8b52ed08d51f982afd49
Jose Espejo <joseespejo@yahoo.com>
2025-06-19 16:53:46 +0100

    feat: add temperature field to main form schema with validation

--- src/lib/schemas/form-schema.ts
+++ src/lib/schemas/form-schema.ts
@@ -5,6 +5,13 @@ export const mainFormSchema = z.object({
   questionCount: z.number().refine((val) => [5, 10, 15, 20].includes(val), {
     message: "Question count must be 5, 10, 15, or 20",
   }),
+  // .default(5),
+  model: z.enum(["gpt-3.5-turbo", "gpt-4o", "gpt-4o-mini"]),
+  // .default("gpt-4o-mini"),
+  temperature: z
+    .number({ required_error: "Required" })
+    .min(0, { message: "Enter a value between 0 and 2.0" })
+    .max(2, { message: "Enter a value between 0 and 2.0" }),
 });
 
 export type MainFormValues = z.infer<typeof mainFormSchema>;
