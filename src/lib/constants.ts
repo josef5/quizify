91e6f5abc9f54c63dbb66cee5555c4079833e0bb
Jose Espejo <joseespejo@yahoo.com>
2025-07-13 16:06:16 +0100

    feat: enhance difficulty settings with detailed descriptions

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,32 +1,37 @@
-import type { MainFormValues } from "./schemas/form-schema";
+import { DifficultyLabels, DifficultySettings } from "@/types";
 
-export const DIFFICULTY_LABELS: Record<MainFormValues["difficulty"], string> = {
+export const DIFFICULTY_LABELS: DifficultyLabels = {
   easy: "Easy",
   medium: "Medium",
   hard: "Hard",
+  harder: "Harder",
 };
 
-export const DIFFICULTY_SETTINGS: Record<
-  MainFormValues["difficulty"],
-  { temperature: number; description: string }
-> = {
+export const DIFFICULTY_SETTINGS: DifficultySettings = {
   easy: {
     temperature: 0.3,
-    description: "basic facts and general knowledge",
+    description:
+      "The questions should be based on knowledge everybody, including a primary school child would know. This includes basic facts and general knowledge.",
   },
   medium: {
     temperature: 0.5,
-    description: "moderate reasoning and conceptual questions",
+    description:
+      "The questions should be based on knowledge a high school student would know, but younger children would not know. This includes moderate reasoning and conceptual questions.",
   },
   hard: {
     temperature: 0.9,
-    description: "analytical or expert-level, niche knowledge",
+    description:
+      "The questions should be based on knowledge a university student would know, but younger students would not know. This includes analytical or expert-level, niche knowledge.",
+  },
+  harder: {
+    temperature: 1.0,
+    description:
+      "The questions should be based on knowledge only a university professor would know. This includes advanced analytical or expert-level, niche knowledge.",
   },
 };
 
 export const QUIZ_PROMPTS_LOCAL_STORAGE_KEY = "quizifyPrompts";
 export const DARK_MODE_LOCAL_STORAGE_KEY = "mode";
-
 export const ANSWER_HOLD_DELAY = 1000; // Delay before showing the next question after an answer is selected
 export const TOAST_OPTIONS = {
   error: {
