8a851cd387d6530678d0c8fe8faea940503a417a
Jose Espejo <joseespejo@yahoo.com>
2025-07-14 21:15:29 +0100

    refactor: add spread props to FinishButton component

--- src/components/ui/finish-button.tsx
+++ src/components/ui/finish-button.tsx
@@ -3,15 +3,17 @@ import { Button } from "./button";
 function FinishButton({
   label,
   onRestart,
+  ...props
 }: {
   label: string;
   onRestart: () => void;
-}) {
+} & React.ComponentProps<typeof Button>) {
   return (
     <Button
       variant={"secondary"}
       onClick={onRestart}
       className="bg-grey-button-bg hover:bg-grey-button-hover text-primary mt-1 mb-24 h-12 w-full cursor-pointer rounded-sm font-bold transition-colors duration-200 ease-in-out"
+      {...props}
     >
       {label}
     </Button>
