79d2a38c8228837bea1c651428e91d96a0358610
Jose Espejo <joseespejo@yahoo.com>
2025-07-14 15:04:46 +0100

    fix: add font-bold class to FinishButton

--- src/components/ui/finish-button.tsx
+++ src/components/ui/finish-button.tsx
@@ -11,7 +11,7 @@ function FinishButton({
     <Button
       variant={"secondary"}
       onClick={onRestart}
-      className="bg-grey-button-bg hover:bg-grey-button-hover text-primary mt-1 mb-24 h-12 w-full cursor-pointer rounded-sm transition-colors duration-200 ease-in-out"
+      className="bg-grey-button-bg hover:bg-grey-button-hover text-primary mt-1 mb-24 h-12 w-full cursor-pointer rounded-sm font-bold transition-colors duration-200 ease-in-out"
     >
       {label}
     </Button>
