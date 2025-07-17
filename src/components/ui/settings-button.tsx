a112ae029065785bb6a76c0ee5a9310a79e0ec94
Jose Espejo <joseespejo@yahoo.com>
2025-07-17 19:04:17 +0100

    style: update border color for SettingsButton component

--- src/components/ui/settings-button.tsx
+++ src/components/ui/settings-button.tsx
@@ -12,7 +12,7 @@ function SettingsButton({
       variant="ghost"
       size={"sm"}
       className={cn(
-        "dark:active:border-brand-1 active:border-brand-1 hover:text-settings-foreground dark:hover:border-settings-accent hover:border-settings-accent text-settings-foreground border-settings-button-border hover:bg-settings-background dark:hover:bg-settings-background bg-settings-background h-6.5 grow cursor-pointer rounded-sm border-2 text-xs",
+        "dark:active:border-brand-1 active:border-brand-1 hover:text-settings-foreground dark:hover:border-settings-accent hover:border-settings-accent text-settings-foreground border-foreground/10 hover:bg-settings-background dark:hover:bg-settings-background bg-settings-background h-6.5 grow cursor-pointer rounded-sm border-2 text-xs",
         className,
       )}
       {...props}
