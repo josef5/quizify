260fd6ba896b5bcbd670f5511327564d824632d6
Jose Espejo <joseespejo@yahoo.com>
2025-07-18 22:44:33 +0100

    a11y: add tabIndex to PromptBadge for improved keyboard navigation

--- src/components/main-form.tsx
+++ src/components/main-form.tsx
@@ -202,6 +202,7 @@ function MainForm({
                           ),
                         );
                       }}
+                      tabIndex={0}
                     >
                       {prompt}
                     </PromptBadge>
