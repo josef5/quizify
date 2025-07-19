f8a52357070b6c08aa6cd82273cb2b4b34c7016c
Jose Espejo <joseespejo@yahoo.com>
2025-07-19 16:02:02 +0100

    feat: add settings story

---
+++ src/stories/settings.stories.tsx
@@ -0,0 +1,31 @@
+import OpenSettingsButton from "@/components/ui/open-settings-button";
+import { useStore } from "@/store/useStore";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { useEffect } from "react";
+import Settings from "../components/settings";
+
+const meta = {
+  component: Settings,
+  render: (): JSX.Element => {
+    const setIsSettingsOpen = useStore((state) => state.setIsSettingsOpen);
+
+    useEffect(() => {
+      setIsSettingsOpen(true);
+
+      return () => setIsSettingsOpen(false);
+    }, [setIsSettingsOpen]);
+
+    return (
+      <>
+        <Settings />
+        <OpenSettingsButton onClick={() => setIsSettingsOpen(true)} />
+      </>
+    );
+  },
+} satisfies Meta<typeof Settings>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
