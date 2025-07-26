0fa7508e88fa73eefa7bb1b2789f9f7bf375f890
Jose Espejo <joseespejo@yahoo.com>
2025-07-26 14:36:02 +0100

    docs: add a note about useFetchQuizDev usage

--- src/hooks/useFetchQuizDev.ts
+++ src/hooks/useFetchQuizDev.ts
@@ -1,6 +1,7 @@
 import { MainFormValues } from "@/lib/schemas/form-schema";
 import sampleQuestions from "@/test/sample-questions.json";
 
+// To use this just change the import to useFetchQuizDev
 export function useFetchQuiz() {
   const fetchQuiz = async ({ questionCount }: MainFormValues) =>
     Promise.resolve({
