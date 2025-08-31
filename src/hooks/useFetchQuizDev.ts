fe9ac706224b0e254a391fb68e0210ba881880bc
Jose Espejo <joseespejo@yahoo.com>
2025-08-31 19:48:34 +0100

    feat: simulate loading delay in dev fetchQuiz function with sleep

--- src/hooks/useFetchQuizDev.ts
+++ src/hooks/useFetchQuizDev.ts
@@ -1,13 +1,17 @@
 import { MainFormValues } from "@/lib/schemas/form-schema";
+import { sleep } from "@/lib/utils";
 import sampleQuestions from "@/test/sample-questions.json";
 
 // To use this just change the import to useFetchQuizDev
 export function useFetchQuiz() {
-  const fetchQuiz = async ({ questionCount }: MainFormValues) =>
-    Promise.resolve({
+  const fetchQuiz = async ({ questionCount }: MainFormValues) => {
+    await sleep(2000);
+
+    return Promise.resolve({
       ...sampleQuestions,
       questions: sampleQuestions.questions.slice(0, questionCount),
     });
+  };
 
   return { fetchQuiz };
 }
